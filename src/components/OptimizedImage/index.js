28b384e5b392b6ab345c6e0f288841f61bd515ef
Kunanon Srisuntiroj <son9912@gmail.com>
2023-08-18 11:33:20 +0700

    Add image validation and optimize image loading
    The changes enhance images preview in blog posts by validating the image url before deciding to use `OptimizedImage` or not. This update manages the use of Cloudinary (an image optimization service) to restrict image transformations only to local images and excludes other external URLs. Also, the Cloudinary instance is created outside the function, which minimizes the overhead of creating a new instance each time the component is re-rendered. Additionally, CSS styling for the images has been tweaked to include rounded corners and for better consistency.
    
    Lastly, PropTypes have been added for better type checking capabilities. These changes improve performance in image loading and provide a better user experience in the blog post view.

--- src/components/OptimizedImage/index.js
+++ src/components/OptimizedImage/index.js
@@ -1,62 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
-    AdvancedImage,
-    lazyload,
-    responsive,
-    placeholder,
+  AdvancedImage,
+  lazyload,
+  responsive,
+  placeholder,
 } from '@cloudinary/react';
 import { Cloudinary } from '@cloudinary/url-gen';
-import { scale } from '@cloudinary/url-gen/actions/resize';
+//import { scale } from '@cloudinary/url-gen/actions/resize';
 import classes from './OptimizedImage.module.css';
 
+// Cloudinary instance created outside the function.
+const cld = new Cloudinary({
+  cloud: {
+    cloudName: 'bytesideone',
+  },
+});
+
 export default function OptimizedImage({ src, alt }) {
-    const cld = new Cloudinary({
-        cloud: {
-            cloudName: 'bytesideone',
-        },
-    });
+  // If the src were not provided, use the placeholder image
+  const imgSrc = (typeof src !== 'string' || src.startsWith('http://') || src.startsWith('https://')) ?
+    'placeholder-image-169-1080_mvmbrm' : src;
 
-    // If the src were not provided, use the placeholder image
-    if (
-        typeof src !== 'string' ||
-        src.startsWith('http://') ||
-        src.startsWith('https://')
-    ) {
-        src = 'placeholder-image-169-1080_mvmbrm';
-    }
+  // Retrieve the image with selected quality and format
+  let image = cld.image(`blog/${imgSrc}`).quality('auto').format('auto');
 
-    // Retrieve the image with selected quality and format
-    let image = cld.image(`blog/${src}`).quality('auto').format('auto');
-
-    // Apply for image transformation
-    // image.resize(
-    //     scale()
-    //         .width(imageWidth)
-    //         .height(imageHeight)
-    //         .gravity(autoGravity())
-    // );
-
-    return (
-        <div
-            className={classes.container}
-            style={{
-                objectFit: 'cover',
-                position: 'relative',
-                overflow: 'hidden',
-                borderRadius: '10px',
-            }}
-        >
-            <AdvancedImage
-                cldImg={image}
-                plugins={[
-                    lazyload({ rootMargin: '10px', threshold: 0.1 }),
-                    responsive({ steps: 200 }),
-                    placeholder({ mode: 'blur' }),
-                ]}
-            />
-            {alt && (
-                <figcaption className={classes.imageCaption}>{alt}</figcaption>
-            )}
-        </div>
-    );
+  return (
+    <div
+      className={classes.container}
+      style={{
+        objectFit: 'cover',
+        position: 'relative',
+        overflow: 'hidden',
+        borderRadius: '10px',
+      }}
+    >
+      <AdvancedImage
+        cldImg={image}
+        plugins={[
+          lazyload({ rootMargin: '10px', threshold: 0.1 }),
+          responsive({ steps: 200 }),
+          placeholder({ mode: 'blur' }),
+        ]}
+      />
+      {alt && (
+        <figcaption className={classes.imageCaption}>{alt}</figcaption>
+      )}
+    </div>
+  );
 }
+
+OptimizedImage.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+};
--- src/theme/BlogPostItem/Header/CoverImage/index.js
+++ src/theme/BlogPostItem/Header/CoverImage/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { useBlogPost } from '@docusaurus/theme-common/internal';
 import OptimizedImage from '@site/src/components/OptimizedImage';
+import classes from './CoverImage.module.css';
 
 const BlogPostItemHeaderCoverImage = () => {
-    const { metadata } = useBlogPost();
-    const { frontMatter } = metadata;
-    const { image } = frontMatter;
+  const { metadata } = useBlogPost();
+  const { frontMatter } = metadata;
+  const { image } = frontMatter;
 
-    return <OptimizedImage src={image} />;
-    // }
+  const isCoverImageValidUrl = (imageUrl) => {
+    //     Check if the cover image is a valid URL or a Cloudinary image ID
+    if (imageUrl.startsWith('http://') || imageUrl.startsWith('https://')) {
+      return true;
+    }
+    return false;
+  };
+
+  return (
+    <>
+      {
+        !isCoverImageValidUrl(image) ?
+          <OptimizedImage src={image} />
+          : <img src={image} className={classes.container} />
+      }
+    </>
+  );
 };
 
 export default BlogPostItemHeaderCoverImage;

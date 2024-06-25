fd25790e9506b6d9dc0d4351e0f71c083eae9410
Kunanon Srisuntiroj <son9912@gmail.com>
2024-06-25 20:00:58 +0700

    feat: Improve OptimizedImage component to handle specific Cloudinary URLs
    - Refactored `getImageSource` to an arrow function.
    - Updated `getImageSource` logic to extract image names from Cloudinary URLs starting with `https://res.cloudinary.com/bytesideone/image/upload/`.
    - Ensured the use of a placeholder image for invalid or non-Cloudinary URLs.
    - Improved code readability and maintainability.

--- src/components/OptimizedImage/index.js
+++ src/components/OptimizedImage/index.js
@@ -1,54 +1,67 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import {
   AdvancedImage,
   lazyload,
   responsive,
   placeholder,
-} from '@cloudinary/react';
-import { Cloudinary } from '@cloudinary/url-gen';
-//import { scale } from '@cloudinary/url-gen/actions/resize';
-import classes from './OptimizedImage.module.css';
+} from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import classes from "./OptimizedImage.module.css";
 
-// Cloudinary instance created outside the function.
+// Cloudinary instance created outside the function to avoid recreation on each render.
 const cld = new Cloudinary({
   cloud: {
-    cloudName: 'bytesideone',
+    cloudName: "bytesideone",
   },
 });
 
+const getImageSource = (src) => {
+  // If src is a Cloudinary link, convert it to use Cloudinary API instead of link.
+  if (
+    typeof src === "string" &&
+    src.startsWith("https://res.cloudinary.com/bytesideone/image/upload/")
+  ) {
+    const parts = src.split("/blog/");
+    return parts.length > 1 ? parts[1] : "placeholder-image-169-1080_mvmbrm";
+  }
+
+  // If src is a valid URL, but not from Cloudinary, use placeholder image.
+  if (src.startsWith("http://") || src.startsWith("https://")) {
+    return "placeholder-image-169-1080_mvmbrm";
+  }
+
+  // Otherwise, return the original src
+  return src;
+};
+
 export default function OptimizedImage({ src, alt }) {
-  // If the src were not provided, use the placeholder image
-  const imgSrc = (typeof src !== 'string' || src.startsWith('http://') || src.startsWith('https://')) ?
-    'placeholder-image-169-1080_mvmbrm' : src;
+  const imgSrc = getImageSource(src);
 
   // Retrieve the image with selected quality and format
-  let image = cld.image(`blog/${imgSrc}`).quality('auto').format('auto');
+  const image = cld.image(`blog/${imgSrc}`).quality("auto").format("auto");
 
   return (
-    <div
-      className={classes.container}
-      style={{
-        objectFit: 'cover',
-        position: 'relative',
-        overflow: 'hidden',
-        borderRadius: '10px',
-      }}
-    >
+    <div className={classes.container} style={containerStyle}>
       <AdvancedImage
         cldImg={image}
         plugins={[
-          lazyload({ rootMargin: '10px', threshold: 0.1 }),
+          lazyload({ rootMargin: "10px", threshold: 0.1 }),
           responsive({ steps: 200 }),
-          placeholder({ mode: 'blur' }),
+          placeholder({ mode: "blur" }),
         ]}
       />
-      {alt && (
-        <figcaption className={classes.imageCaption}>{alt}</figcaption>
-      )}
+      {alt && <figcaption className={classes.imageCaption}>{alt}</figcaption>}
     </div>
   );
 }
+
+const containerStyle = {
+  objectFit: "cover",
+  position: "relative",
+  overflow: "hidden",
+  borderRadius: "10px",
+};
 
 OptimizedImage.propTypes = {
   src: PropTypes.string,
